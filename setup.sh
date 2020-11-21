e3c2249de69cce48027de01e3bf4e980141894ac
kwroro <74576831+kwroro@users.noreply.github.com>
2020-11-21 05:19:16 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,129 @@
+#!/system/xbin/bash
+
+apt update -y
+
+apt upgrade -y
+
+apt install curl -y
+
+apt install python -y
+
+apt install python2 -y
+
+apt install unzip -y
+
+pip install mechanize -y
+
+apt install git -y
+
+pkg install cowsay -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install git -y
+
+pkg install nano -y
+
+pkg install figlet -y
+
+pkg install update -y
+
+pkg install upgrad -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install git -y
+
+pkg install ruby -y
+
+pkg install nmap -y
+
+pkg install unzip -y
+
+pkg install nano -y
+
+apt update -y
+
+ 
+
+apt upgrade -y
+
+pkg install update -y
+
+pkg install upgrade -y
+
+apt-get update -y
+
+apt-get upgrade -y
+
+apt install php -y
+
+apt install python -y
+
+apt install python2 -y
+
+apt install git -y
+
+apt install golang -y
+
+apt install host -y
+
+apt install nano -y
+
+apt install havij -y
+
+apt install hydra -y
+
+apt install wireshark -y
+
+apt install  -y
+
+pkg install figlet -y
+
+pkg install wget -y
+
+pkg install wget -y
+
+pkg install python2 -y
+
+pkg install python2-dev -y
+
+apt install wireshark -y
+
+pkg install cowsay -y
+
+pkg install toilet -y
+
+pkg install ruby -y
+
+pkg install help -y
+
+gem install lolcat -y
+
+pkg install curl -y
+
+pkg install wage  -y
+
+pkg install unzip -y
+
+pkg install openssh -y
+
+pkg install tor -y
+
+pkg install uzip -y
+
+pkg install net-tools -y
+
+pkg install unrar -y
+
+pkg install clang -y
+
+pkg install w3m -y
+
+pkg insta -y 
+
+xdg-open https://www.youtube.com/channel/UCwmJg-hAMnu2mK_5qJ5sv7w
